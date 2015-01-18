2ecccb920d01e7514fcc14fc83b5b603f0ea8ea5
MarcusBarbu <mgb.barbu@gmail.com>
2015-01-17 20:11:23 -0500

    Added simple auton, some new methods, started logging

---
+++ RecycleBot/src/org/usfirst/frc/team2601/robot/commands/SampleAuton.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team2601.robot.commands;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class SampleAuton extends CommandGroup {
+    
+    public  SampleAuton() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+    	addSequential(new AutonDriveForward(4.3));
+    	Timer.delay(0.5);
+    	addSequential(new AutonDriveBackward(5.0));
+    	Timer.delay(0.5);
+    	addParallel(new ExtendPiston());
+    	addSequential(new AutonPID(210.0));
+    	Timer.delay(0.5);
+    	addSequential(new RetractPiston());
+    	
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
--- RecycleBot/src/org/usfirst/frc/team2601/robot/subsystems/Drivetrain.java
+++ RecycleBot/src/org/usfirst/frc/team2601/robot/subsystems/Drivetrain.java
@@ -6,10 +6,17 @@ import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.PIDSource;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import org.usfirst.frc.team2601.robot.Constants;
 import org.usfirst.frc.team2601.robot.commands.DumbDrive;
+import java.util.logging.Logger;
+
 import edu.wpi.first.wpilibj.PIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.DriverStation;
 import org.usfirst.frc.team2601.robot.Robot;
 
 
@@ -19,13 +26,16 @@ import org.usfirst.frc.team2601.robot.Robot;
  */
 public class Drivetrain extends Subsystem {
     //set up all of the things involved in the drivetrain
+	String filename = "/logs/drivetrainlog.csv";
 	CANTalon leftTalon = new CANTalon(Constants.leftTalonAddress);
 	CANTalon rightTalon = new CANTalon(Constants.rightTalonAddress);
 	RobotDrive drive = new RobotDrive(leftTalon, rightTalon);
 	Encoder leftEncoder = new Encoder(Constants.leftEncoderPortI,Constants.leftEncoderPortII, true, Encoder.EncodingType.k4X);
+	DriverStation driver;
 	
 	//setup first version of PID controller
 	PIDController control = new PIDController(Constants.kP,Constants.kI,Constants.kD, leftEncoder, leftTalon);
+	boolean CSVstart;
 	
 	SmartDashboard dash;
 
@@ -45,12 +55,16 @@ public class Drivetrain extends Subsystem {
     	Constants.kP = Robot.table.getNumber("P");
     	Constants.kI = Robot.table.getNumber("I");
     	Constants.kD = Robot.table.getNumber("D");
+    	
+    }
+    public void getSetpoint(){
     	Constants.setpoint = Robot.table.getNumber("setpoint");
     }
     
     public void startPID(){
     	//get PID values from Net Tables
     	getPIDvalues();
+    	getSetpoint();
     	
     	//set up encoders
     	leftEncoder.setDistancePerPulse(Constants.distancePerPulse);
@@ -94,8 +108,92 @@ public class Drivetrain extends Subsystem {
     	control.disable();
     }
     
+    public void distanceDriveForwardPID(){
+    	//get PID values from Net Tables
+    	getPIDvalues();
+    	
+    	//set up encoders
+    	leftEncoder.setDistancePerPulse(Constants.distancePerPulse);
+    	leftEncoder.setPIDSourceParameter(PIDSource.PIDSourceParameter.kDistance);
+    	
+    	//set up PID loop parameters
+    	control.setPID(Constants.kP, Constants.kI, Constants.kD);
+    	control.setSetpoint(Constants.setpoint);
+    	control.setAbsoluteTolerance(Constants.absoluteTolerance);
+    	control.setOutputRange(Constants.minOutput, Constants.maxOutput);
+    	
+    	//enable loop, match motors
+    	control.enable();
+    	matchMotors();
+    	
+    	//check if it's ok to stop
+    	if (control.onTarget()){
+    		stopPID();
+    		return;
+    	} 	
+    }
     
-}
+    public void moveForward(){
+    	leftTalon.set(0.75);
+    	rightTalon.set(0.75);
+    }
+    
+    public void moveBackward(){
+    	leftTalon.set(-0.75);
+    	rightTalon.set(-0.75);
+    }
+    
+    public void logStart(String param){
+    	try
+    	{
+    	    FileWriter writer = new FileWriter(filename);
+     
+    	    writer.append("Time");
+    	    writer.append(',');
+    	    writer.append(param);
+    	    writer.append('\n');
+     
+    	    //generate whatever data you want
+     
+    	    writer.flush();
+    	    writer.close();
+    	}
+    	catch(IOException e)
+    	{
+    	     e.printStackTrace();
+    	} 
+    	CSVstart = true;
+        }
+    
+    public void logData(double val){
+    	driver = DriverStation.getInstance();
+    	if (CSVstart){
+    		try
+    		{
+    		    FileWriter writer = new FileWriter(filename);
+    	 String value = Double.toString(val);
+    		    writer.append(Double.toString(driver.getMatchTime()));
+    		    writer.append(',');
+    		    writer.append(value);
+    		    writer.append('\n');
+    	 
+    		    writer.flush();
+    		    writer.close();
+    		}
+    		catch(IOException e)
+    		{
+    		     e.printStackTrace();
+    		} 
+    	    }
+    	}
+    }
+	
+    
+    
+    
+    
+    
+
 
 
 
